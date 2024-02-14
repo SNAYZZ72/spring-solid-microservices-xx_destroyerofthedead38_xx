f656ce5cb8b8735725769b5d96dc7638580db6a7
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-02-14 15:30:33 +0100

    customer

--- customer/src/main/java/fr/mbds/customer/entities/Customer.java
+++ customer/src/main/java/fr/mbds/customer/entities/Customer.java
@@ -1,4 +1,17 @@
 package fr.mbds.customer.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }
