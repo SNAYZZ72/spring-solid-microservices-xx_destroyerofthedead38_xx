16af0538b8c32341afaaaad88c8b8a7d52d16398
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-02-14 15:37:25 +0100

    customer @entity ect

--- customer/src/main/java/fr/mbds/customer/entities/Customer.java
+++ customer/src/main/java/fr/mbds/customer/entities/Customer.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
+
+@Setter @Getter @NoArgsConstructor @AllArgsConstructor @ToString
 @Entity
 public class Customer {
     @Id
