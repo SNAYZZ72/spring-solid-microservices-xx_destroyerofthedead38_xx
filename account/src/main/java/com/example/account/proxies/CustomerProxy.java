3aa56f2f48ffd028e62fb57a2b802b04c0ff7a81
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-02-15 16:41:17 +0100

    added circuit breaker on account service

--- account/src/main/java/com/example/account/proxies/CustomerProxy.java
+++ account/src/main/java/com/example/account/proxies/CustomerProxy.java
@@ -1,6 +1,7 @@
 package com.example.account.proxies;
 
 import com.example.account.models.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.Getter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,26 @@ import java.util.List;
 @FeignClient(name = "customer", url = "http://localhost:8091/")
 public interface CustomerProxy {
     @GetMapping("/customer/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     List<Customer> getCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not available");
+        customer.setLastName("Not available");
+        customer.setEmail("Not available");
+
+        return customer;
+    }
+
+    default List<Customer> getAllCustomers(Exception exception) {
+        return List.of();
+    }
+
 
 }
